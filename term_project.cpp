6adfdd9f4d083b4e0aaf7739836193c5aa662ef5
moneal386 <92813614+moneal386@users.noreply.github.com>
2021-10-19 13:01:58 -0400

    Update term_project.cpp

--- term_project.cpp
+++ term_project.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
 #include <string>
 using namespace std;
+void newStudentRecord();
+void deleteStudentRecord();
+void displayStudentRecords();
+void showRecordByID();
+void editStudentRecord();
+    
 
 int main()
+    {
     int opt;
   
-  //input
-  cout <<"=========================================================================================================\n";
-  cout <<"Please select one of the following options:\n";
-  cout <<"[1]: Add a new student record.\n";
-  cout <<"[2]: Delete a student record.\n";
-  cout <<"[3]: Display all student records.\n";
-  cout <<"[4]: Show a student record by ID.\n";
-  cout <<"[5]: Edit a student record.\n";
-  cout <<"[6]: Exit.\n";
-  cout <<"=========================================================================================================\n";
-  cin >> opt;
+     //input
+     do 
+        {
+          cout<<"=========================================================================================================\n";
+          cout <<"Please select one of the following options:\n";
+          cout <<"[1]: Add a new student record.\n";
+          cout <<"[2]: Delete a student record.\n";
+          cout <<"[3]: Display all student records.\n";
+          cout <<"[4]: Show a student record by ID.\n";
+          cout <<"[5]: Edit a student record.\n";
+          cout <<"[6]: Exit.\n";
+          cout <<"=========================================================================================================\n";
+         
+          cin >> opt;
+        }
+    while (opt != 6);
+  
+    void newStudentRecord()
+    {
+        
+    }
+    void deleteStudentRecord()
+    {
+        
+    }
+    void displayStudentRecords()
+    {
+        
+    }
+    void showRecordByID()
+    {
+    
+    }
+    void editStudentRecord()
+    {
+  
+    }
+  
+
+
+
 
 
