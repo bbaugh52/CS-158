033a72f4d78cdcbe10614d9c35f75479ffd3e79f
bbaugh52 <91982078+bbaugh52@users.noreply.github.com>
2021-10-19 12:40:53 -0400

    Create term_project.cpp

---
+++ term_project.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+    int opt;
+  
+  //input
+  cout <<"=========================================================================================================\n";
+  cout <<"Please select one of the following options:\n";
+  cout <<"[1]: Add a new student record.\n";
+  cout <<"[2]: Delete a student record.\n";
+  cout <<"[3]: Display all student records.\n";
+  cout <<"[4]: Show a student record by ID.\n";
+  cout <<"[5]: Edit a student record.\n";
+  cout <<"[6]: Exit.\n";
+  cout <<"=========================================================================================================\n";
+  cin >> opt;
+
+
