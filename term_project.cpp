5f628609b3d42e119844e5c166ad64006b2c111e
moneal386 <92813614+moneal386@users.noreply.github.com>
2021-10-22 15:12:44 -0400

    Update term_project.cpp

--- term_project.cpp
+++ term_project.cpp
@@ -5,6 +5,7 @@ using namespace std;
 // matts notes delete before submitting - use return function to output from a function call
 // if function returns a value define a data type (string/int etc) if it doesnt use void
 // when changing arrays, its not done sequentially, ask the user which one they want to do it to by either number or ID
+// record number is good name for request
 
 // functions
 void newStudentRecord();
@@ -28,31 +29,50 @@ int main()
 
 	//input
 	do
-			cout << "=========================================================================================================\n";
-			cout << "Please select one of the following options:\n";
-			cout << "[1]: Add a new student record.\n";
-			cout << "[2]: Delete a student record.\n";
-			cout << "[3]: Display all student records.\n";
-			cout << "[4]: Show a student record by ID.\n";
-			cout << "[5]: Edit a student record.\n";
-			cout << "[6]: Exit.\n";
-			cout << "=========================================================================================================\n";
-
-			cin >> opt;
-
-			if (opt = 1)
-			{
-				newStudentRecord();
-			}
-			else if
-			{
-				deleteStudentRecord();
-			}
-			
-			while (opt != 6);
-}
-void newStudentRecord()
 	{
+		cout << "=========================================================================================================\n";
+		cout << "Please select one of the following options:\n";
+		cout << "[1]: Add a new student record.\n";
+		cout << "[2]: Delete a student record.\n";
+		cout << "[3]: Display all student records.\n";
+		cout << "[4]: Show a student record by ID.\n";
+		cout << "[5]: Edit a student record.\n";
+		cout << "[6]: Exit.\n";
+		cout << "=========================================================================================================\n";
+
+		cin >> opt;
+
+		if (opt = 1)
+		{
+			newStudentRecord();
+		}
+		else if (opt = 2)
+		{
+			deleteStudentRecord();
+		}
+		else if (opt = 3)
+		{
+			displayStudentRecords();
+		}
+		else if (opt = 4)
+		{
+			showRecordByID();
+		}
+		else if (opt = 5)
+		{
+			editStudentRecord();
+		}
+		else
+		{
+			cout << "You have entered an invalid option, please close the program and try again." << endl;
+		}
+			
+	} 
+	while (opt != 6);
+}
+
+void newStudentRecord()
+{
 	int studentID;
 	int requestedDataSlot;
 	cout << "Please enter the following information to add a new student record: \n";
@@ -69,31 +89,37 @@ void newStudentRecord()
 	cin >> midtermGrade[i];
 	cout << "Enter the final grade: \n";
 	cin >> finalGrade[i];
-	}
+}
 
-	void deleteStudentRecord()
-	{
-		int a
-			cout >> "Please input a student ID to delete" >> endl;
-		cin << a
-			a =
-	}
-	void displayStudentRecords()
-	{
+void deleteStudentRecord()
+{
+	int recordNumber;
+		cout << "Please enter a record number to delete." << endl;
+		cin >> recordNumber;
 
-	}
-	void showRecordByID()
-	{
+		studentID[recordNumber] = 0;
+		studentFirstName[recordNumber] = "";
+		studentLastName[recordNumber] = "";
+		midtermGrade[recordNumber] = 0;
+		finalGrade[recordNumber] = 0;
 
-	}
-	void editStudentRecord()
-	{
+}
+void displayStudentRecords()
+{
 
-	}
+}
+void showRecordByID()
+{
 
-	void newStudentRecord()
-	{
-	}
+}
+void editStudentRecord()
+{
+
+}
+
+void newStudentRecord()
+{
+}
 
 
 
